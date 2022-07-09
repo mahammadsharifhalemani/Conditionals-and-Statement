95c1ed33b00a6717ccdaea9123bedfbd2805bebe
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-07-09 13:15:23 +0530

    third commit

---
+++ practise3.js
@@ -0,0 +1,13 @@
+//If Number
+var var1 = 50;
+var var2 = "42F";
+if (isNaN(var1)){
+    console.log('50 is not a number');
+} else {
+    console.log('50 is a valid number');
+}if (isNaN(var2)){
+    console.log('42F is not a number');
+} else {
+    console.log('42F is a valid number');
+}
+//Output: 50 is a valid number 42F is not a number
