4221aefd60c8e12ee2816d6c5e566c043562afde
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-07-09 13:07:50 +0530

    second commit

---
+++ practise2.js
@@ -0,0 +1,15 @@
+//Even or Odd
+var num3 = 17.5;
+var num4 = 42;
+if (17.5%2==0)
+{
+    console.log('17.5 is even');
+}else {
+    console.log('17.5 is odd');
+}if (42%2==0)
+{
+    console.log('42 is even');
+}else {
+    console.log('42 is odd');
+}
+//Output: 17.5 is odd 42 is even
