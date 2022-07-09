3f5dd0f28c0de848acdfb651d6afa55557b29fb5
mahammadsharifhalemani <sharif.halemani@gmail.com>
2022-07-09 12:59:52 +0530

    first commit

---
+++ practise1.js
@@ -0,0 +1,12 @@
+//Dispay large integer
+var num1 = 5;
+var num2 = 10;
+if (5>=10)
+{
+    console.log('The larger of 5 and 10 is 5');
+}
+else if (10>=10);
+{
+    console.log('The larger of 5 and 10 is 10');
+}
+//Output: The larger of 5 and 10 is 10
